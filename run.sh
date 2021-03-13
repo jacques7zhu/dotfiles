a7349aa1df403ec0f18cd5267f40a968407c65a5
unknown <jacques7zhu@126.com>
2021-03-13 20:49:32 +0800

    add init.vim

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ PWD=$(pwd)
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/gitconfig ~/.gitconfig
 ln -s $PWD/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.config/nvim/ && ln -s $PWD/init.vim ~/.config/nvim/init.vim
 ln -s $PWD/ctags ~/.ctags
 ln -s $PWD/tmux.conf ~/.tmux.conf
 ln -s $PWD/tmux ~/.tmux
