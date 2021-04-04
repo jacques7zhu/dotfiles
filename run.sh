c3a808f3322acde5d452e9e1a224f7cfe53567a1
YuanjieZhuKolmostar <yuanjie.zhu@kolmostar.com>
2021-04-04 19:34:44 +0800

    add config for docker

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 PWD=$(pwd)
-ln -s $PWD/vimrc ~/.vimrc
-ln -s $PWD/gitconfig ~/.gitconfig
-ln -s $PWD/config.fish ~/.config/fish/config.fish
-mkdir -p ~/.config/nvim/ && ln -s $PWD/init.vim ~/.config/nvim/init.vim
-ln -s $PWD/ctags ~/.ctags
-ln -s $PWD/tmux.conf ~/.tmux.conf
-ln -s $PWD/tmux ~/.tmux
+ln -sf $PWD/vimrc ~/.vimrc
+ln -sf $PWD/gitconfig ~/.gitconfig
+ln -sf $PWD/config.fish ~/.config/fish/config.fish
+mkdir -p ~/.config/nvim/ && ln -sf $PWD/init.vim ~/.config/nvim/init.vim
+ln -sf $PWD/ctags ~/.ctags
+ln -sf $PWD/tmux.conf ~/.tmux.conf
+ln -sf $PWD/tmux ~/.tmux
+ln -sf $PWD/docker_config.json ~/.docker/config.json
